12293900098d25f630d6e3a1ba5b0fe90816ecca
NeakNoura <mrrnora05@gmail.com>
2025-02-06 16:14:49 +0700

    first commit

---
+++ app/Http/Controllers/Products/ProductsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Products;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Product\Product;
+
+
+
+class ProductsController extends Controller
+{
+    public function sigleProducts($id) {
+
+        $product = Product::find($id);
+
+        return view('products.productsingle', compact('product'));
+    }
+}
